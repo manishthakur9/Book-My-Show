bffe9258cd696f5eee0df3b06f39bca5af1e2fa1
manishthakur9 <139028482+manishthakur9@users.noreply.github.com>
2023-12-25 10:20:33 +0530

    Add files via upload

--- user-controller.js
+++ user-controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+ import User from "../models/User";
 
 export const getAllusers = async(req, res, next) => {
 let users;
@@ -8,12 +8,36 @@ let users;
     return next(err);
 }
 
-
-
 if(!users) {
     return res.status(500).json({message:"Unexpected Error Occured"})
 }
-
     return res.status(200).json({ users});
 
+};
+
+export const singup = async(req, res, next) =>{
+    const{name,email,password } = req.body;
+    if(
+        !name &&
+        name.trim()==="" &&
+        !email &&
+        email.trim()==="" && 
+        !password && 
+        password.trim()===""
+      )  {
+        return res.status(422).json({message: "Invalid Inputs"});
+      }
+
+      let user;
+      try{ 
+
+        user = new User({ name, email, password});
+        user = user.save();
+      }catch(err) {
+        return next(err);
+      }
+      if(!user) {
+          return res.status(500).json({ message: "Unexpected Error Occured"});
+      }
+      return res.status(201).json({ user });
 };
--- user-routes.js
+++ user-routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllusers } from "../controllers/user-controller"
+import { getAllusers, singup } from "../controllers/user-controller";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllusers); 
+userRouter.post("/signup", singup); 
 
 export default userRouter;
